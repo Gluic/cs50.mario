2e8b7291efaf95ebbd9a3e0072e1d90439bbe9eb
Gluic <gluic.hp@gmail.com>
2016-05-01 12:19:03 -0400

    final

--- mario.c
+++ mario.c
@@ -1,28 +1,46 @@
 #include <stdio.h>
 #include <cs50.h>
-
+/*
+* The Tower of Mario 
+* prints a tower of "#"
+* height input by user (from 0 to 23)
+**/
 int main(void)
 {
-    string block = "##";
-    string block1 = "#";
-    printf ("Input height from 1 to 23 of the Mario tower: ");
+    int x = 1;// init a value for loop 
+    //get a height of tower
+    printf ("Input height from of the Mario tower: ");
     int height = GetInt();
     
-    while (height < 1 || height > 23)
-    {
-            printf ("Bad input. Try: ");
-            height = GetInt();
-     }
-     for (int k = 0;k < height;k++)
+    //check if value of height is negative or more than 24
+    while (height < 0 || height > 23)         
+                {
+                printf ("Input height from of the Mario tower: ");
+                height = GetInt();
+                }
+    if (height == 0)
+                return 0;
+   
+    //loop for build the tower
+    for (int k = height;k > 0;k--)
         {
-            int h = k;
+            //print a spaces before # 
+            int h = k - 1;
             while (h > 0)
             {
-              printf ("%s",block1);
+              printf (" ");
               h = h - 1;   
             }
-            
-            printf ("%s\n",block);
+            //print a # 
+            h = x;
+            while (h > 0)
+            {
+              printf ("#");
+              h = h - 1;   
+            }
+            //print a last # with new line and grow cicle number
+            x = x + 1;
+            printf ("#\n");
             
         }
 }
